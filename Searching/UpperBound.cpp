cf4910ac5f9fdaf57f51dd5342f20d28ae69cb63
Razuanul Hoque Nolin <razuanulnolin@gmail.com>
2026-01-09 14:55:35 +0600

    Add upper bound binary search implementation in C++
    Introduces UpperBound.cpp, which implements a function to find the upper bound index of a target in a sorted array using binary search. Includes a main function demonstrating usage with sample data.

---
+++ Searching/UpperBound.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include<vector>
+using namespace std;
+
+int upperBound(vector<int>& arr, int target){
+    int left = 0;
+    int right = arr.size();
+
+    while(left<right){
+        int mid = left+((right-left)/2);
+
+        if(target>=arr[mid]){
+            left = mid +1;
+        }else{
+            right = mid;
+        }  
+    }
+    return left;
+}
+
+int main(){
+    vector<int> arr = {1,3,7,7,7,23,45,45,45,67,78,78,90};
+    int target = 45;
+
+    int foundIndex = upperBound(arr,target);
+
+    cout<<"Target first occured at Index no: "<<foundIndex<<endl;
+
+    return 0;
+}
