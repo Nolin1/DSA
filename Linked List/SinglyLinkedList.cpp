fe2a3e9a86dfda0f0f2d09187a9234b18d6fb34a
Razuanul Hoque Nolin <razuanulnolin@gmail.com>
2026-01-10 00:34:45 +0600

    Add basic singly linked list implementation in C++
    Introduces SinglyLinkedList.cpp with node structure and functions to add nodes to the front and end, as well as print the list. Includes a main function demonstrating usage.

---
+++ Linked List/SinglyLinkedList.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+using namespace std;
+
+struct Node{
+    int data;
+    Node* nextNodeAddress;
+};
+
+Node* head = nullptr;
+
+Node* createNode(int data);
+void addNodeToFront(int data);
+void addNodeToEnd(int data);
+void deleteAtFront();
+void deleteAtEnd();
+void deleteAfter(int data);
+void updateNode(int data, int target);
+void printList();
+
+Node* createNode(int data){
+    Node* temp = new Node();
+    temp->data = data;
+    temp->nextNodeAddress = nullptr;
+
+    return temp;
+}
+
+void addNodeToFront(int data){
+    
+    if(head == nullptr){
+        head = createNode(data);
+    }else{
+        Node* temp = createNode(data);
+        temp->nextNodeAddress = head;
+        head = temp;
+    }
+}
+
+void addNodeToEnd(int data){
+
+    if(head == nullptr){
+        head = createNode(data);
+    }else{
+        Node* temp = createNode(data);
+        Node* lastNode = head;
+        while(lastNode->nextNodeAddress != nullptr){
+            lastNode = lastNode ->nextNodeAddress;
+        }
+        lastNode->nextNodeAddress = temp;
+    }
+}
+
+void printList(){
+    Node* printer = head;
+    while(printer != nullptr){
+        cout<<printer->data<<" -> ";
+        printer = printer->nextNodeAddress;
+    }
+    cout<<"NULL"<<endl;
+}
+
+int main(){
+    
+    addNodeToFront(34);
+    addNodeToFront(23);
+    addNodeToFront(12);
+    addNodeToEnd(1);
+    printList();
+
+    return 0;
+}
