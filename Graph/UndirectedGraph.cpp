be60c817b93f351a5d2c50f06e235cfabfb50a4d
Razuanul Hoque Nolin <razuanulnolin@gmail.com>
2026-01-18 22:30:47 +0600

    Add UndirectedGraph implementation in C++
    Introduces a Graph class for undirected graphs with methods to add edges and print adjacency lists. Includes a main function demonstrating usage with a sample graph.

---
+++ Graph/UndirectedGraph.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include<vector>
+#include<list>
+using namespace std;
+
+class Graph{
+    int V;
+    list<int>* l;
+
+public:
+    Graph(int V){
+        this -> V = V;
+        l = new list<int> [V];
+    }
+
+    void addEdge(int u, int v){
+        l[u].push_back(u);
+        l[v].push_back(v);
+    }
+
+    void printList(){
+        for(int i = 0 ; i< V; i++){
+            cout<< i <<": ";
+            for(int neibour:l[i]){
+                cout<<neibour<< " ";
+            }
+            cout<<endl;
+        }
+    }
+};
+
+int main(){
+    Graph g (5);
+
+    g.addEdge(0,1);
+    g.addEdge(1,2);
+    g.addEdge(1,3);
+    g.addEdge(2,3);
+    g.addEdge(2,4);
+
+    g.printList();
+
+    return 0;
+}
